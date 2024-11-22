d8db1321f5563440067ca62b7d83879237c97beb
Ana <gutierrez.baron.ana@iescamas.es>
2024-11-22 18:36:36 +0100

    Completados ejercicios 1 y 2

--- estructuras/src/main/java/com/ejercicio/estructuras/controlador/Controlador.java
+++ estructuras/src/main/java/com/ejercicio/estructuras/controlador/Controlador.java
@@ -9,7 +9,27 @@ public class Controlador {
 	
 	@PostMapping("/parImpar")
 	public String ejercicio1(@RequestParam int numero){
-		String cadena = "Hola Mundo";
-		return cadena;
+		String respuesta = "";
+		if (numero % 2 == 0) {
+			respuesta += "El numero " + numero + " es par";
+		} else {
+			respuesta += "El numero " + numero + " es impar";
+		}
+		return "<h1>" + respuesta + "</h1>";
+	}
+	
+	@PostMapping("/tablaMultiplicar")
+	public String ejercicio2(@RequestParam int numero) {
+		String abertura = "<table>";
+		String cierre = "</table>";
+		String cabecera = "<tr><th>Múltiplo</th><th>Resultado</th></tr>";
+		String respuesta = abertura + cabecera;
+		for (int i = 1; i<=10; i++) {
+			int cociente = numero * i;
+			respuesta += "<tr><td>" + i + "</td><td>" + cociente + "</td></tr>";
+		}
+		respuesta+= cierre;
+		
+		return respuesta;
 	}
 }
